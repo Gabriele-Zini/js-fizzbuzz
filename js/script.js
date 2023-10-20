b1c3f7d4bbbd581a30050d9aa674bd962d9c6bf7
Gabriele-Zini <gabrielezini845@gmail.com>
2023-10-20 14:29:45 +0200

    style: dom layout

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-// PROGRAM LOGICÙ
+const elementRow = document.querySelector(".row");
+let elementGrid = "";
+
+// PROGRAM LOGIC
 
 for (let i = 1; i <= 100; i++) {
   let result;
+  let color;
   if ((i % 3 === 0) & (i % 5 === 0)) {
     console.log("FizzBuzz");
-    result = "fizz-buzz";
+    result = "FizzBuzz";
+    color= "fizz-buzz"
   } else if (i % 3 === 0) {
     console.log("Fizz");
-    result = "fizz";
+    result = "Fizz";
+    color= "fizz"
   } else if (i % 5 === 0) {
     console.log("Buzz");
-    result = "buzz";
+    result = "Buzz";
+    color= "buzz"
   } else {
     console.log(i);
+    result = `${i}`;
+    color="normal"
   }
+  elementGrid += `<div class="${color} col-3 col-md-2 ms_col d-flex card">${result}</div>`;
 }
+
+elementRow.innerHTML = elementGrid;
